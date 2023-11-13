b560df32ff33a7c07902f487a8f68734c4715ed0
Arshad Anas <arshadanas900+github@gmail.com>
2023-11-13 16:39:07 -0500

    ScoringMeasurements.java for backdrop and pixel dimensions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/placementalg/ScoringMeasurements.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.control.placementalg;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class ScoringMeasurements {
+    public static double
+            BACKDROP_TOP_Y_BLUE = 45,
+            BACKDROP_TOP_Y_RED = -25,
+            PIXEL_WIDTH = 0,
+            PIXEL_HEIGHT = 0,
+            SCORING_X = 47;
+}
