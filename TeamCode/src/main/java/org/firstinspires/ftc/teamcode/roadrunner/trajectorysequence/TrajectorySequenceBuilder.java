173f5fcc17c0e43e5ab3a74408e7d6f5eeedabec
Arshad Anas <79271473+Arshad-Anas@users.noreply.github.com>
2023-11-14 08:57:56 -0500

    Add missing import
    Mirroring https://github.com/acmerobotics/road-runner-quickstart/commit/e7ee954c10dbf4a0118981336b90ce094385c07f

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/trajectorysequence/TrajectorySequenceBuilder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/trajectorysequence/TrajectorySequenceBuilder.java
@@ -26,6 +26,7 @@ import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.sequencesegm
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class TrajectorySequenceBuilder {
