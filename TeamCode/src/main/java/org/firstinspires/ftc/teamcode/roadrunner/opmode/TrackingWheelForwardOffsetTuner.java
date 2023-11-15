c1892b08f6ce30b8181d0a1e2f0a042c0af7d44a
Arshad Anas <arshadanas900+github@gmail.com>
2023-11-14 20:15:42 -0500

    "Standard" to "ThreeWheel"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/opmode/TrackingWheelForwardOffsetTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/opmode/TrackingWheelForwardOffsetTuner.java
@@ -13,7 +13,7 @@ import com.qualcomm.robotcore.util.RobotLog;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.internal.system.Misc;
 import org.firstinspires.ftc.teamcode.roadrunner.SampleMecanumDrive;
-import org.firstinspires.ftc.teamcode.roadrunner.StandardTrackingWheelLocalizer;
+import org.firstinspires.ftc.teamcode.roadrunner.ThreeWheelTrackingLocalizer;
 
 /**
  * This routine determines the effective forward offset for the lateral tracking wheel.
@@ -47,7 +47,7 @@ public class TrackingWheelForwardOffsetTuner extends LinearOpMode {
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        if (!(drive.getLocalizer() instanceof StandardTrackingWheelLocalizer)) {
+        if (!(drive.getLocalizer() instanceof ThreeWheelTrackingLocalizer)) {
             RobotLog.setGlobalErrorMsg("StandardTrackingWheelLocalizer is not being set in the "
                     + "drive class. Ensure that \"setLocalizer(new StandardTrackingWheelLocalizer"
                     + "(hardwareMap));\" is called in SampleMecanumDrive.java");
@@ -83,7 +83,7 @@ public class TrackingWheelForwardOffsetTuner extends LinearOpMode {
                 drive.update();
             }
 
-            double forwardOffset = StandardTrackingWheelLocalizer.FORWARD_OFFSET +
+            double forwardOffset = ThreeWheelTrackingLocalizer.FORWARD_OFFSET +
                     drive.getPoseEstimate().getY() / headingAccumulator;
             forwardOffsetStats.add(forwardOffset);
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/util/LogFiles.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/util/LogFiles.java
@@ -19,7 +19,7 @@ import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
 import org.firstinspires.ftc.teamcode.roadrunner.DriveConstants;
 import org.firstinspires.ftc.teamcode.roadrunner.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.roadrunner.SampleTankDrive;
-import org.firstinspires.ftc.teamcode.roadrunner.StandardTrackingWheelLocalizer;
+import org.firstinspires.ftc.teamcode.roadrunner.ThreeWheelTrackingLocalizer;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -84,11 +84,11 @@ public final class LogFiles {
         public double tankHeadingI = SampleTankDrive.HEADING_PID.kI;
         public double tankHeadingD = SampleTankDrive.HEADING_PID.kD;
 
-        public double trackingTicksPerRev = StandardTrackingWheelLocalizer.TICKS_PER_REV;
-        public double trackingWheelRadius = StandardTrackingWheelLocalizer.WHEEL_RADIUS;
-        public double trackingGearRatio = StandardTrackingWheelLocalizer.GEAR_RATIO;
-        public double trackingLateralDistance = StandardTrackingWheelLocalizer.LATERAL_DISTANCE;
-        public double trackingForwardOffset = StandardTrackingWheelLocalizer.FORWARD_OFFSET;
+        public double trackingTicksPerRev = ThreeWheelTrackingLocalizer.TICKS_PER_REV;
+        public double trackingWheelRadius = ThreeWheelTrackingLocalizer.WHEEL_RADIUS;
+        public double trackingGearRatio = ThreeWheelTrackingLocalizer.GEAR_RATIO;
+        public double trackingLateralDistance = ThreeWheelTrackingLocalizer.LATERAL_DISTANCE;
+        public double trackingForwardOffset = ThreeWheelTrackingLocalizer.FORWARD_OFFSET;
 
         public RevHubOrientationOnRobot.LogoFacingDirection LOGO_FACING_DIR = DriveConstants.LOGO_FACING_DIR;
         public RevHubOrientationOnRobot.UsbFacingDirection USB_FACING_DIR = DriveConstants.USB_FACING_DIR;
