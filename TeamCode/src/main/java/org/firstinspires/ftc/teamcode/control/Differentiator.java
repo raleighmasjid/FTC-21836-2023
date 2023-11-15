7ff8e699fe313e23c0522362601262ad9f170d77
Arshad Anas <arshadanas900+github@gmail.com>
2023-11-14 20:36:14 -0500

    remove filter from differentiator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/Differentiator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/Differentiator.java
@@ -2,23 +2,11 @@ package org.firstinspires.ftc.teamcode.control;
 
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.control.filters.FIRLowPassFilter;
-
 public class Differentiator {
 
     private double lastValue = Double.NaN, derivative = 0.0;
 
     private final ElapsedTime timer = new ElapsedTime();
-
-    public final FIRLowPassFilter filter;
-
-    public Differentiator() {
-        this(new FIRLowPassFilter());
-    }
-
-    public Differentiator(FIRLowPassFilter filter) {
-        this.filter = filter;
-    }
 
     public double getDerivative(double newValue) {
 
@@ -26,7 +14,7 @@ public class Differentiator {
         timer.reset();
 
         if (dt != 0.0 && !Double.isNaN(lastValue)) {
-            derivative = filter.calculate((newValue - lastValue) / dt);
+            derivative = (newValue - lastValue) / dt;
         }
 
         lastValue = newValue;
