5b869b715aa4cf384d179a3a136a33ccfd38ddc3
Arshad Anas <arshadanas900+github@gmail.com>
2023-11-15 20:50:21 -0500

    Drivetrain teleop programmed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MainTeleOp.java
@@ -2,9 +2,12 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.drivetrains.MecanumDrivetrain;
 
 import java.util.List;
 
@@ -14,6 +17,8 @@ public class MainTeleOp extends LinearOpMode {
     // Declare objects:
     MultipleTelemetry myTelemetry;
     List<LynxModule> hubs;
+    MecanumDrivetrain drivetrain;
+    GamepadEx Gamepad1, Gamepad2;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -26,15 +31,33 @@ public class MainTeleOp extends LinearOpMode {
         hubs = hardwareMap.getAll(LynxModule.class);
         for (LynxModule hub : hubs) hub.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
 
+        drivetrain = new MecanumDrivetrain(hardwareMap);
+
+        Gamepad1 = new GamepadEx(gamepad1);
+        Gamepad2 = new GamepadEx(gamepad2);
+
         waitForStart();
+        drivetrain.imu.start();
 
         // Control loop:
         while (opModeIsActive()) {
             // Manually clear old sensor data from the last loop:
             for (LynxModule hub : hubs) hub.clearBulkCache();
+            // Read sensors + gamepads:
+            Gamepad1.readButtons();
+            Gamepad2.readButtons();
+            drivetrain.readIMU();
+
+            // Field-centric drive dt with control stick inputs:
+            drivetrain.run(
+                    Gamepad1.getLeftX(),
+                    Gamepad1.getLeftY(),
+                    Gamepad1.getRightX()
+            );
 
             // Push telemetry data to multiple outputs (set earlier):
             myTelemetry.update();
         }
+        drivetrain.imu.interrupt();
     }
 }
