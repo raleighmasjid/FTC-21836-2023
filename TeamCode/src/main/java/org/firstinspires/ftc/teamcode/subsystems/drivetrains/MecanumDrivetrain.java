50da9dac522cfe9d3d49b069979e8a683f4628b9
Arshad Anas <arshadanas900+github@gmail.com>
2023-11-17 18:12:38 -0500

    reverse right-side motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drivetrains/MecanumDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drivetrains/MecanumDrivetrain.java
@@ -26,6 +26,7 @@ import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
@@ -112,6 +113,8 @@ public class MecanumDrivetrain extends MecanumDrive {
         }
 
         // TODO: reverse any motors using DcMotor.setDirection()
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
 
         List<Integer> lastTrackingEncPositions = new ArrayList<>();
         List<Integer> lastTrackingEncVels = new ArrayList<>();
