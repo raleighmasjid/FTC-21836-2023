ab0266133f37772c74c064f02a936d02252d5dac
Arshad Anas <arshadanas900+github@gmail.com>
2023-12-02 12:16:26 -0500

    HSV.inRange(HSV, HSV)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/gainmatrices/HSV.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/gainmatrices/HSV.kt
@@ -4,4 +4,14 @@ data class HSV @JvmOverloads constructor(
     @JvmField var hue: Float = 0.0F,
     @JvmField var saturation: Float = 0.0F,
     @JvmField var value: Float = 0.0F,
-)
+) {
+
+    fun inRange(min:HSV, max:HSV): Boolean {
+
+        val hueRange = min.hue..max.hue
+        val saturationRange = min.saturation..max.saturation
+        val valueRange = min.value..max.value
+
+        return hueRange.contains(hue) && saturationRange.contains(saturation) && valueRange.contains(value)
+    }
+}
