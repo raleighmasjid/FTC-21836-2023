97bc8acd471db30707a299e4d9a8b0dd0ddc6c87
Arshad Anas <arshadanas900+github@gmail.com>
2023-12-03 11:06:44 -0500

    individual buttons for colors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/centerstage/placementalg/GUITesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/centerstage/placementalg/GUITesting.java
@@ -1,5 +1,10 @@
 package org.firstinspires.ftc.teamcode.subsystems.centerstage.placementalg;
 
+import static org.firstinspires.ftc.teamcode.subsystems.centerstage.placementalg.Pixel.Color.EMPTY;
+import static org.firstinspires.ftc.teamcode.subsystems.centerstage.placementalg.Pixel.Color.GREEN;
+import static org.firstinspires.ftc.teamcode.subsystems.centerstage.placementalg.Pixel.Color.PURPLE;
+import static org.firstinspires.ftc.teamcode.subsystems.centerstage.placementalg.Pixel.Color.WHITE;
+import static org.firstinspires.ftc.teamcode.subsystems.centerstage.placementalg.Pixel.Color.YELLOW;
 import static java.lang.Thread.sleep;
 
 import java.util.Objects;
@@ -19,7 +24,11 @@ final class GUITesting {
             else if (Objects.equals(key, "f")) backdropGUI.incrementX();
             else if (Objects.equals(key, "s")) backdropGUI.decrementX();
             else if (Objects.equals(key, "d")) backdropGUI.decrementY();
-            else if (Objects.equals(key, "c")) backdropGUI.flipColor();
+            else if (Objects.equals(key, "z")) backdropGUI.changeTo(EMPTY);
+            else if (Objects.equals(key, "x")) backdropGUI.changeTo(WHITE);
+            else if (Objects.equals(key, "c")) backdropGUI.changeTo(PURPLE);
+            else if (Objects.equals(key, "v")) backdropGUI.changeTo(YELLOW);
+            else if (Objects.equals(key, "b")) backdropGUI.changeTo(GREEN);
 
             backdropGUI.print();
 
