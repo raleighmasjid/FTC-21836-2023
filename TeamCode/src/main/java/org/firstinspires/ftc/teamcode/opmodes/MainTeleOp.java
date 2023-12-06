dfcdeb3d9683d688554d320f8579fe7d77994bfb
Arshad Anas <arshadanas900+github@gmail.com>
2023-12-05 22:18:03 -0500

    pressed() --> keyPressed()

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MainTeleOp.java
@@ -15,7 +15,7 @@ import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Trigger.RIGHT_TRIGGER;
 import static org.firstinspires.ftc.teamcode.opmodes.MainAuton.gamepadEx1;
 import static org.firstinspires.ftc.teamcode.opmodes.MainAuton.gamepadEx2;
 import static org.firstinspires.ftc.teamcode.opmodes.MainAuton.mTelemetry;
-import static org.firstinspires.ftc.teamcode.opmodes.MainAuton.pressed;
+import static org.firstinspires.ftc.teamcode.opmodes.MainAuton.keyPressed;
 import static org.firstinspires.ftc.teamcode.opmodes.MainAuton.robot;
 import static org.firstinspires.ftc.teamcode.subsystems.centerstage.Intake.IntakingHeight.FIVE_STACK;
 import static org.firstinspires.ftc.teamcode.subsystems.centerstage.Intake.IntakingHeight.FLOOR;
@@ -59,9 +59,9 @@ public final class MainTeleOp extends LinearOpMode {
         // Get gamepad 1 button input, locks slow mode, and saves "red" boolean for teleop configuration:
         while (opModeInInit()) {
             gamepadEx1.readButtons();
-            if (pressed(1, RIGHT_BUMPER))   robot.drivetrain.toggleSlowModeLock();
-            if (pressed(1, B))              robot.isRed = true;
-            if (pressed(1, X))              robot.isRed = false;
+            if (keyPressed(1, RIGHT_BUMPER))   robot.drivetrain.toggleSlowModeLock();
+            if (keyPressed(1, B))              robot.isRed = true;
+            if (keyPressed(1, X))              robot.isRed = false;
             mTelemetry.addLine((robot.drivetrain.isSlowModeLocked() ? "SLOW" : "NORMAL") + " mode");
             mTelemetry.addLine((robot.isRed ? "RED" : "BLUE") + " alliance");
             mTelemetry.update();
@@ -76,12 +76,12 @@ public final class MainTeleOp extends LinearOpMode {
             gamepadEx2.readButtons();
 
             // Reset current heading as per these keybinds:
-            if (pressed(1, DPAD_UP))            robot.drivetrain.setCurrentHeading(0);
-            if (pressed(1, DPAD_LEFT))          robot.drivetrain.setCurrentHeading(PI / 2);
-            if (pressed(1, DPAD_DOWN))          robot.drivetrain.setCurrentHeading(PI);
-            if (pressed(1, DPAD_RIGHT))         robot.drivetrain.setCurrentHeading(-PI / 2);
+            if (keyPressed(1, DPAD_UP))            robot.drivetrain.setCurrentHeading(0);
+            if (keyPressed(1, DPAD_LEFT))          robot.drivetrain.setCurrentHeading(PI / 2);
+            if (keyPressed(1, DPAD_DOWN))          robot.drivetrain.setCurrentHeading(PI);
+            if (keyPressed(1, DPAD_RIGHT))         robot.drivetrain.setCurrentHeading(-PI / 2);
 
-            if (pressed(1, A))                  robot.deposit.dropPixel();
+            if (keyPressed(1, A))                  robot.deposit.dropPixel();
 
             robot.intake.setMotorPower(
                     gamepadEx1.getTrigger(RIGHT_TRIGGER) - gamepadEx1.getTrigger(LEFT_TRIGGER)
@@ -89,27 +89,27 @@ public final class MainTeleOp extends LinearOpMode {
 
             if (gamepadEx2.isDown(LEFT_BUMPER)) {
 
-                if (pressed(2, DPAD_UP))        gui.up();
-                if (pressed(2, DPAD_DOWN))      gui.down();
-                if (pressed(2, DPAD_LEFT))      gui.left();
-                if (pressed(2, DPAD_RIGHT))     gui.right();
+                if (keyPressed(2, DPAD_UP))        gui.up();
+                if (keyPressed(2, DPAD_DOWN))      gui.down();
+                if (keyPressed(2, DPAD_LEFT))      gui.left();
+                if (keyPressed(2, DPAD_RIGHT))     gui.right();
 
-                if (pressed(2, Y))              gui.update(YELLOW);
-                if (pressed(2, X))              gui.update(PURPLE);
-                if (pressed(2, A))              gui.update(GREEN);
-                if (pressed(2, B))              gui.update(WHITE);
-                if (pressed(2, RIGHT_BUMPER))   gui.update(EMPTY);
+                if (keyPressed(2, Y))              gui.update(YELLOW);
+                if (keyPressed(2, X))              gui.update(PURPLE);
+                if (keyPressed(2, A))              gui.update(GREEN);
+                if (keyPressed(2, B))              gui.update(WHITE);
+                if (keyPressed(2, RIGHT_BUMPER))   gui.update(EMPTY);
 
             } else {
 
-                if (pressed(2, DPAD_DOWN))      robot.lift.decrementRow();
-                if (pressed(2, DPAD_UP))        robot.lift.incrementRow();
+                if (keyPressed(2, DPAD_DOWN))      robot.lift.decrementRow();
+                if (keyPressed(2, DPAD_UP))        robot.lift.incrementRow();
 
-                if (pressed(2, Y))              robot.intake.setHeight(FIVE_STACK);
-                if (pressed(2, X))              robot.intake.setHeight(FOUR_STACK);
-                if (pressed(2, B))              robot.intake.setHeight(THREE_STACK);
-                if (pressed(2, A))              robot.intake.setHeight(TWO_STACK);
-                if (pressed(2, RIGHT_BUMPER))   robot.intake.setHeight(FLOOR);
+                if (keyPressed(2, Y))              robot.intake.setHeight(FIVE_STACK);
+                if (keyPressed(2, X))              robot.intake.setHeight(FOUR_STACK);
+                if (keyPressed(2, B))              robot.intake.setHeight(THREE_STACK);
+                if (keyPressed(2, A))              robot.intake.setHeight(TWO_STACK);
+                if (keyPressed(2, RIGHT_BUMPER))   robot.intake.setHeight(FLOOR);
 
             }
 
