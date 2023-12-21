119738833f205998ff44afab5847594f9c23ae10
Arshad Anas <arshadanas900+github@gmail.com>
2023-12-20 21:27:43 -0500

    apply SMARTDAMP kD formula to PIDGains

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/gainmatrices/PIDGains.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/gainmatrices/PIDGains.kt
@@ -1,8 +1,23 @@
 package org.firstinspires.ftc.teamcode.control.gainmatrices
 
-data class PIDGains @JvmOverloads constructor(
+import kotlin.Double.Companion.POSITIVE_INFINITY
+import kotlin.math.*
+
+data class PIDGains
+
+@JvmOverloads
+constructor(
     @JvmField var kP: Double = 0.0,
     @JvmField var kI: Double = 0.0,
     @JvmField var kD: Double = 0.0,
-    @JvmField var maxOutputWithIntegral: Double = Double.POSITIVE_INFINITY,
-)
+    @JvmField var maxOutputWithIntegral: Double = POSITIVE_INFINITY,
+) {
+
+    fun criticallyDamped(gains: FeedforwardGains, percentOver: Double = 0.0): PIDGains {
+        kP = max(gains.kV.pow(2) / (4 * gains.kA), kP)
+        val overshoot: Double = percentOver / 100.0
+        val zeta: Double = if (overshoot == 0.0) 1.0 else -ln(overshoot) / sqrt(PI.pow(2) + ln(overshoot).pow(2))
+        kD = 2 * zeta * sqrt(gains.kA * kP) - gains.kV
+        return this
+    }
+}
