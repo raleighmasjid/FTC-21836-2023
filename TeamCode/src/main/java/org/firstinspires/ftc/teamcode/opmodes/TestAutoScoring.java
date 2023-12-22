d33aa6d633b52039c6bc905f3522ba06dbfbe682
Arshad Anas <arshadanas900+github@gmail.com>
2023-12-22 17:51:12 -0500

    simplify TestAutoScoring with BackdropScanner object

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestAutoScoring.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestAutoScoring.java
@@ -1,18 +1,12 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
 import static org.firstinspires.ftc.teamcode.opmodes.MainAuton.robot;
-import static org.firstinspires.ftc.teamcode.subsystems.centerstage.Deposit.Paintbrush.TIME_DROP_FIRST;
-import static org.firstinspires.ftc.teamcode.subsystems.centerstage.Deposit.Paintbrush.TIME_DROP_SECOND;
-import static org.firstinspires.ftc.teamcode.subsystems.centerstage.placementalg.Pixel.Color.WHITE;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.teamcode.subsystems.centerstage.Robot;
-import org.firstinspires.ftc.teamcode.subsystems.centerstage.placementalg.Backdrop;
-import org.firstinspires.ftc.teamcode.subsystems.centerstage.placementalg.Pixel;
+import org.firstinspires.ftc.teamcode.subsystems.centerstage.placementalg.BackdropScanner;
 
 
 @TeleOp(group = "21836 B")
@@ -29,61 +23,9 @@ public final class TestAutoScoring extends LinearOpMode {
 
         robot = new Robot(hardwareMap);
 
-        Pixel[] placements = new Pixel[]{
-                new Pixel(X1, Y1, WHITE),
-                new Pixel(X2, Y2, WHITE)
-        };
-        Backdrop latestScan = new Backdrop();
+        BackdropScanner scanner = new BackdropScanner(robot);
 
-        Pose2d scoringPos1 = placements[0].toPose2d();
-        Pose2d scoringPos2 = placements[1].toPose2d();
-
-        Pose2d startPose = robot.drivetrain.getPoseEstimate();
-
-        boolean sameScoringLocation = scoringPos1.epsilonEquals(scoringPos2);
-
-        TrajectorySequence scoringTrajectory =
-                sameScoringLocation ?
-                                robot.drivetrain.trajectorySequenceBuilder(startPose)
-                                        .addTemporalMarker(() -> {
-                                            robot.deposit.lift.setTargetRow(placements[1].y);
-                                        })
-                                        .lineToSplineHeading(scoringPos2)
-                                        .addTemporalMarker(() -> {
-                                            robot.deposit.paintbrush.dropPixels(2);
-                                            latestScan.add(placements[1]);
-                                        })
-                                        .waitSeconds(TIME_DROP_SECOND)
-                                        .addTemporalMarker(() -> {
-//                                            trajectoryReady = false;
-                                        })
-                                        .build() :
-                                robot.drivetrain.trajectorySequenceBuilder(startPose)
-                                        .addTemporalMarker(() -> {
-                                            robot.deposit.lift.setTargetRow(placements[0].y);
-                                        })
-                                        .lineToSplineHeading(scoringPos1)
-                                        .addTemporalMarker(() -> {
-                                            robot.deposit.paintbrush.dropPixels(1);
-                                            latestScan.add(placements[0]);
-                                        })
-                                        .waitSeconds(TIME_DROP_FIRST)
-                                        .addTemporalMarker(() -> {
-                                            robot.deposit.lift.setTargetRow(placements[1].y);
-                                        })
-                                        .lineToConstantHeading(scoringPos2.vec())
-                                        .addTemporalMarker(() -> {
-                                            robot.deposit.paintbrush.dropPixels(2);
-                                            latestScan.add(placements[1]);
-                                        })
-                                        .waitSeconds(TIME_DROP_SECOND)
-                                        .addTemporalMarker(() -> {
-//                                            trajectoryReady = false;
-                                        })
-                                        .build()
-        ;
-
-        robot.drivetrain.followTrajectorySequenceAsync(scoringTrajectory);
+        robot.drivetrain.followTrajectorySequenceAsync(scanner.getScoringTrajectory());
 
         waitForStart();
 
