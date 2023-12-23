667b0ac6dbb44aa5d03a160844d90f39e472f374
Arshad Anas <arshadanas900+github@gmail.com>
2023-12-23 13:38:09 -0500

    Create TestSMARTDAMP.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/gainmatrices/TestSMARTDAMP.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.control.gainmatrices;
+
+import static org.firstinspires.ftc.teamcode.subsystems.centerstage.Deposit.Lift.PERCENT_OVERSHOOT;
+import static org.firstinspires.ftc.teamcode.subsystems.centerstage.Deposit.Lift.feedforwardGains;
+import static org.firstinspires.ftc.teamcode.subsystems.centerstage.Deposit.Lift.pidGains;
+
+public final class TestSMARTDAMP {
+
+    public static void main(String[] args) {
+
+        pidGains.criticallyDamp(feedforwardGains, PERCENT_OVERSHOOT);
+
+        System.out.println(pidGains.kP);
+        System.out.println(pidGains.kD);
+    }
+}
