4bdda019c45b1663dbb16b625574ea3916db44ab
Arshad Anas <arshadanas900+github@gmail.com>
2023-12-24 19:53:05 -0500

    Tune intake angles

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/centerstage/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/centerstage/Intake.java
@@ -36,9 +36,9 @@ import org.firstinspires.ftc.teamcode.subsystems.utilities.ThreadedColorSensor;
 public final class Intake {
 
     public static double
-            ANGLE_PIVOT_OFFSET = 0,
+            ANGLE_PIVOT_OFFSET = 15,
             ANGLE_PIVOT_FLOOR_CLEARANCE = 0,
-            ANGLE_PIVOT_TRANSFERRING = 235,
+            ANGLE_PIVOT_TRANSFERRING = 197,
             ANGLE_LATCH_INTAKING = 105,
             ANGLE_LATCH_LOCKED = 159,
             ANGLE_LATCH_TRANSFERRING = 0,
