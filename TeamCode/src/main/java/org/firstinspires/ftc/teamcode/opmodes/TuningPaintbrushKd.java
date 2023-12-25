c0513e673b9d37fa53b5b25ad8b45b28e470e2b2
Arshad Anas <arshadanas900+github@gmail.com>
2023-12-24 19:52:58 -0500

    Add testing opmode for Deposit pivot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TuningPaintbrushKd.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.X;
+import static org.firstinspires.ftc.teamcode.opmodes.MainAuton.gamepadEx1;
+import static org.firstinspires.ftc.teamcode.opmodes.MainAuton.keyPressed;
+import static org.firstinspires.ftc.teamcode.subsystems.centerstage.Deposit.Paintbrush.ANGLE_PIVOT_OFFSET;
+import static org.firstinspires.ftc.teamcode.subsystems.utilities.SimpleServoPivot.getAxonServo;
+import static org.firstinspires.ftc.teamcode.subsystems.utilities.SimpleServoPivot.getReversedServo;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.utilities.BulkReader;
+import org.firstinspires.ftc.teamcode.subsystems.utilities.SimpleServoPivot;
+
+@TeleOp(group = "21836 B")
+public final class TuningPaintbrushKd extends LinearOpMode {
+
+    SimpleServoPivot pivot;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        BulkReader bulkReader = new BulkReader(hardwareMap);
+
+        pivot = new SimpleServoPivot(
+                ANGLE_PIVOT_OFFSET,
+                ANGLE_PIVOT_OFFSET + 120,
+                getAxonServo(hardwareMap, "deposit left"),
+                getReversedServo(getAxonServo(hardwareMap, "deposit right"))
+        );
+
+        // Initialize gamepads:
+        gamepadEx1 = new GamepadEx(gamepad1);
+
+        waitForStart();
+
+        // Control loop:
+        while (opModeIsActive()) {
+            bulkReader.bulkRead();
+            gamepadEx1.readButtons();
+
+            pivot.updateAngles(ANGLE_PIVOT_OFFSET, ANGLE_PIVOT_OFFSET + 120);
+
+            if (keyPressed(1, X)) pivot.toggle();
+
+            pivot.run();
+        }
+    }
+}
