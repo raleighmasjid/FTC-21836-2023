08abb4e0627034d447b332a8f79972d6c0e4403d
Arshad Anas <arshadanas900+github@gmail.com>
2023-12-29 21:39:35 -0500

    static computeKd() method for using SMARTDAMP anywhere

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/gainmatrices/PIDGains.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/gainmatrices/PIDGains.kt
@@ -14,13 +14,18 @@ constructor(
 ) {
 
     @JvmOverloads
-    fun computeKd(gains: FeedforwardGains, percentOver: Double = 0.0): PIDGains {
-        val overshoot: Double = percentOver / 100.0
-        val zeta: Double = if (overshoot <= 0.0) 1.0 else -ln(overshoot) / sqrt(PI.pow(2) + ln(overshoot).pow(2))
-        kD = max(
-            0.0,
-            2 * zeta * sqrt(gains.kA * kP) - gains.kV
-        )
+    fun computeKd(gains: FeedforwardGains, percentOvershoot: Double = 0.0): PIDGains {
+        kD = computeKd(kP, gains.kV, gains.kA, percentOvershoot)
         return this
     }
+}
+
+@JvmOverloads
+fun computeKd(kP: Double, kV: Double, kA: Double, percentOvershoot: Double = 0.0): Double {
+    val overshoot: Double = percentOvershoot / 100.0
+    val zeta: Double = if (overshoot <= 0.0) 1.0 else -ln(overshoot) / sqrt(PI.pow(2) + ln(overshoot).pow(2))
+    return max(
+        2 * zeta * sqrt(kA * kP) - kV,
+        0.0
+    )
 }
