f2e560896b5c82c4dbcd075c2aa7ef0e00fbb340
Arshad Anas <arshadanas900+github@gmail.com>
2024-01-02 15:07:42 -0500

    print auton pixel supplier output for meepmeep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/centerstage/placementalg/AutonPlacementVisualizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/centerstage/placementalg/AutonPlacementVisualizer.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystems.centerstage.placementalg;
 
+import static org.firstinspires.ftc.teamcode.subsystems.centerstage.placementalg.AutonPixelSupplier.Randomization.LEFT;
 import static org.firstinspires.ftc.teamcode.subsystems.centerstage.placementalg.AutonPixelSupplier.Randomization.randomizations;
 
 import java.util.ArrayList;
@@ -14,10 +15,19 @@ public final class AutonPlacementVisualizer {
                 Backdrop backdrop = new Backdrop();
 
                 for (int j = 0; j < 15; j++) {
-                    Pixel placement = optimalPlacements.get(j);
-                    backdrop.add(placement);
+                    backdrop.add(optimalPlacements.get(j));
                 }
                 backdrop.print();
+            }
+        }
+    }
+
+    static class Bah {
+        public static void main(String... args) {
+//            Backdrop backdrop = new Backdrop();
+            for (Pixel placement : AutonPixelSupplier.getPlacements(LEFT, false)) {
+                System.out.println(placement.x + ", " + placement.y + ", " + placement.color.name());
+//                backdrop.add(placement).print();
             }
         }
     }
