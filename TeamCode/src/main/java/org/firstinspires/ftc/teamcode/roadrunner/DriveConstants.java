77b3bb7944a7b66f03ef016c274388066a104b10
Arshad Anas <arshadanas900+github@gmail.com>
2024-01-13 16:40:13 -0500

    tune velocity feedforward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/DriveConstants.java
@@ -57,9 +57,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.014;
-    public static double kA = 0;
-    public static double kStatic = 0;
+    public static double kV = 0.012;
+    public static double kA = 0.004;
+    public static double kStatic = 0.0125;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -69,7 +69,7 @@ public class DriveConstants {
      * inches.
      */
     public static double MAX_VEL = 50;
-    public static double MAX_ACCEL = 60;
+    public static double MAX_ACCEL = 50;
     public static double MAX_ANG_VEL = Math.toRadians(60);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
