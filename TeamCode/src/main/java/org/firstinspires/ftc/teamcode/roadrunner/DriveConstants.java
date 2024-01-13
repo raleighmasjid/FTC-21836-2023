29aac185a5f11aeb54f52d26388125f8602c8f70
Yousuf Siddiqui <yousufsiddiqui1707@gmail.com>
2024-01-13 12:36:01 -0500

    started tuning velocity feedforward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/DriveConstants.java
@@ -57,7 +57,7 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
+    public static double kV = 0.014;
     public static double kA = 0;
     public static double kStatic = 0;
 
@@ -68,8 +68,8 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 30;
-    public static double MAX_ACCEL = 30;
+    public static double MAX_VEL = 50;
+    public static double MAX_ACCEL = 60;
     public static double MAX_ANG_VEL = Math.toRadians(60);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/ThreeWheelTrackingLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/ThreeWheelTrackingLocalizer.java
@@ -33,10 +33,10 @@ public class ThreeWheelTrackingLocalizer extends com.acmerobotics.roadrunner.loc
             TICKS_PER_REV = 8192,
             WHEEL_RADIUS = 0.748031, // in
             GEAR_RATIO = 1, // output (wheel) speed / input (encoder) speed
-            X_MULTIPLIER = 1.005,
+            X_MULTIPLIER = 1.0050905,
             Y_MULTIPLIER = 0.999969,
-            LATERAL_DISTANCE = 14.0008, // in; distance between the left and right wheels
-            FORWARD_OFFSET = -0.86938; // in; offset of the lateral wheel
+            LATERAL_DISTANCE = 13.999525, // in; distance between the left and right wheels
+            FORWARD_OFFSET = 0.86938; // in; offset of the lateral wheel
 
     private final Encoder leftEncoder, rightEncoder, frontEncoder;
 
