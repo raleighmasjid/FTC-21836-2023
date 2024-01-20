48aa5038611c907cd832f01e3f20b15aa27ff0e2
Arshad Anas <arshadanas900+github@gmail.com>
2024-01-20 15:48:56 -0500

    make State() attributes final

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/motion/State.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/motion/State.kt
@@ -7,10 +7,10 @@ data class State
 
 @JvmOverloads
 constructor(
-    @JvmField var x: Double = 0.0,
-    @JvmField var v: Double = 0.0,
-    @JvmField var a: Double = 0.0,
-    @JvmField var j: Double = 0.0,
+    @JvmField val x: Double = 0.0,
+    @JvmField val v: Double = 0.0,
+    @JvmField val a: Double = 0.0,
+    @JvmField val j: Double = 0.0,
 ) {
 
     operator fun plus(other: State): State {
