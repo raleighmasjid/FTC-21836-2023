de9c5d46bc11fbfb88ab1c9b25f9003207be3ab8
Arshad Anas <arshadanas900+github@gmail.com>
2024-02-01 16:28:47 -0500

    fully retract intake for going to the stacks during auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MainAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MainAuton.java
@@ -103,14 +103,14 @@ public final class MainAuton extends LinearOpMode {
     private static void driveToStack1(TrajectorySequenceBuilder sequence, Intake.Height height) {
         sequence
                 .addTemporalMarker(() -> {
-                    robot.intake.setVertical(true);
+                    robot.intake.setRequiredIntakingAmount(0);
                     robot.intake.setHeight(height);
                 })
                 .setTangent(MainAuton.startPose.byAlliance().heading)
                 .lineTo(MainAuton.enteringBackstage.byAlliance().toPose2d().vec())
                 .setTangent(LEFT)
                 .addTemporalMarker(() -> {
-                    robot.intake.setVertical(false);
+                    robot.intake.setRequiredIntakingAmount(2);
                 })
                 .splineTo(stackPos(1, height).vec(), LEFT)
         ;
