de36c843254a646aef434751270ec1e92d736d84
Arshad Anas <arshadanas900+github@gmail.com>
2024-02-07 19:13:41 -0500

    update initial color amounts editing keybinds

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutomatedTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutomatedTeleOp.java
@@ -7,6 +7,7 @@ import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.DPAD_UP;
 import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.LEFT_BUMPER;
 import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.RIGHT_BUMPER;
 import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.X;
+import static org.firstinspires.ftc.teamcode.control.vision.pipelines.placementalg.PlacementCalculator.initialColors;
 import static org.firstinspires.ftc.teamcode.opmodes.MainAuton.gamepadEx1;
 import static org.firstinspires.ftc.teamcode.opmodes.MainAuton.gamepadEx2;
 import static org.firstinspires.ftc.teamcode.opmodes.MainAuton.keyPressed;
@@ -14,7 +15,6 @@ import static org.firstinspires.ftc.teamcode.opmodes.MainAuton.mTelemetry;
 import static org.firstinspires.ftc.teamcode.opmodes.MainAuton.robot;
 import static org.firstinspires.ftc.teamcode.opmodes.MainTeleOp.teleOpControls;
 import static org.firstinspires.ftc.teamcode.opmodes.MainTeleOp.teleOpInit;
-import static org.firstinspires.ftc.teamcode.control.vision.pipelines.placementalg.PlacementCalculator.initialColors;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -51,10 +51,10 @@ public final class AutomatedTeleOp extends LinearOpMode {
 
                 if (gamepadEx2.isDown(LEFT_BUMPER)) {
                     if (gamepadEx2.isDown(RIGHT_BUMPER)) robot.autoScoringManager.reset();
-                    if (keyPressed(2, DPAD_RIGHT)) selectedColor = (selectedColor + 1) % 3;
-                    if (keyPressed(2, DPAD_LEFT)) selectedColor = (selectedColor - 1) % 3;
-                    if (keyPressed(2, DPAD_UP)) initialColors[selectedColor]++;
-                    if (keyPressed(2, DPAD_DOWN)) initialColors[selectedColor]--;
+                    if (keyPressed(2, DPAD_DOWN)) selectedColor = (selectedColor + 1) % 3;
+                    if (keyPressed(2, DPAD_UP)) selectedColor = (selectedColor - 1) % 3;
+                    if (keyPressed(2, DPAD_RIGHT)) initialColors[selectedColor]++;
+                    if (keyPressed(2, DPAD_LEFT)) initialColors[selectedColor]--;
                 }
                 teleOpControls();
             }
@@ -62,7 +62,9 @@ public final class AutomatedTeleOp extends LinearOpMode {
             robot.run();
 
             mTelemetry.addData("Scoring mode", autoScoring ? "auto" : "manual");
-            mTelemetry.addLine(Pixel.Color.get(selectedColor).name() + " pixels left: " + initialColors[selectedColor]);
+            for (int i = 0; i < initialColors.length; i++) mTelemetry.addLine(
+                Pixel.Color.get(i).name() + ": " + initialColors[i] + (i == selectedColor ? " <" : "")
+            );
             mTelemetry.addLine();
             robot.printTelemetry();
             mTelemetry.update();
