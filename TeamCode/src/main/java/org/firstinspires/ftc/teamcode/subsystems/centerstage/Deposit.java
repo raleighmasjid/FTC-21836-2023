54fa3d98b489d393dc550faf2c15ecc75e5e37b0
Arshad Anas <79271473+Arshad-Anas@users.noreply.github.com>
2024-02-11 15:17:24 -0500

    tuning transfer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/centerstage/Deposit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/centerstage/Deposit.java
@@ -204,8 +204,8 @@ public final class Deposit {
     public static final class Paintbrush {
 
         public static double
-                ANGLE_PIVOT_OFFSET = 10,
-                ANGLE_PIVOT_SCORING = 115,
+                ANGLE_PIVOT_OFFSET = 8.375,
+                ANGLE_PIVOT_SCORING = 127,
                 ANGLE_CLAW_OPEN = 13,
                 ANGLE_CLAW_CLOSED = 50,
                 ANGLE_HOOK_OPEN = 8,
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/centerstage/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/centerstage/Intake.java
@@ -43,9 +43,9 @@ import org.firstinspires.ftc.teamcode.subsystems.utilities.sensors.ColorSensor;
 public final class Intake {
 
     public static double
-            ANGLE_PIVOT_OFFSET = 11,
+            ANGLE_PIVOT_OFFSET = 10,
             ANGLE_PIVOT_FLOOR_CLEARANCE = 3,
-            ANGLE_PIVOT_TRANSFERRING = 195.25,
+            ANGLE_PIVOT_TRANSFERRING = 195,
             ANGLE_PIVOT_VERTICAL = 110,
             ANGLE_LATCH_INTAKING = 105,
             ANGLE_LATCH_LOCKED = 159,
