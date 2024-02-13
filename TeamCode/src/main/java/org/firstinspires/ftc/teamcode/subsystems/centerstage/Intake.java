3bf66558d495526fd68cb4866b96735136ad3563
Arshad Anas <arshadanas900+github@gmail.com>
2024-02-13 15:31:17 -0500

    skip timer wait if retracting intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/centerstage/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/centerstage/Intake.java
@@ -254,7 +254,7 @@ public final class Intake {
 
             case PIXEL_1_SETTLING:
 
-                if (timer.seconds() >= TIME_PIXEL_1_SETTLING) state = HAS_1_PIXEL;
+                if (!isIntaking || timer.seconds() >= TIME_PIXEL_1_SETTLING) state = HAS_1_PIXEL;
                 else break;
 
             case HAS_1_PIXEL:
