57c6d10f4c207806365a8e982ce28aa870af0a3a
Arshad Anas <79271473+Arshad-Anas@users.noreply.github.com>
2024-02-20 18:34:07 -0500

    adjust floor clearance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/centerstage/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/centerstage/Intake.java
@@ -39,7 +39,7 @@ public final class Intake {
 
     public static double
             ANGLE_PIVOT_OFFSET = 9,
-            ANGLE_PIVOT_FLOOR_CLEARANCE = 3,
+            ANGLE_PIVOT_FLOOR_CLEARANCE = 2.5,
             ANGLE_PIVOT_TRANSFERRING = 196.5,
             ANGLE_PIVOT_VERTICAL = 110,
             ANGLE_LATCH_INTAKING = 105,
