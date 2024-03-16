e6b89eda9420b9a6b0cc11f3ca825a725277efb6
Arshad Anas <arshadanas900+github@gmail.com>
2024-03-16 13:54:58 -0400

    add comments to swerve kinematics

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/motion/swerve/SwerveKinematics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/motion/swerve/SwerveKinematics.java
@@ -23,34 +23,45 @@ public final class SwerveKinematics {
         iX = drive.getX(),
         t = drive.getHeading() / hypot(WIDTH, LENGTH),
 
+        // Calculate rotation component vectors
         tY = t * LENGTH,
         tX = t * WIDTH,
 
+        // Combine component vectors into total x and y vectors (per pod)
         a = iY - tY,
         b = iY + tY,
         c = iX - tX,
         d = iX + tX,
 
+        // Get velocity vector (hypotenuse) of total x and y vectors (per pod)
         vBR = hypot(a, d),
         vBL = hypot(a, c),
         vFR = hypot(b, d),
         vFL = hypot(b, c),
 
+        // Calculate pod angles with total x and y vectors (per pod)
         aBR = atan2(a, d),
         aBL = atan2(a, c),
         aFR = atan2(b, d),
         aFL = atan2(b, c),
 
+        // Get max to normalize wheel velocities
         max = max(1.0, max(
                 max(abs(vBR), abs(vBL)),
                 max(abs(vFR), abs(vFL))
         ));
 
+        // Normalize motor powers to [-1, 1]
+        vBR /= max;
+        vBL /= max;
+        vFR /= max;
+        vFL /= max;
+
         return new SwervePodState[]{
-            new SwervePodState(vBR / max, aBR),
-            new SwervePodState(vBL / max, aBL),
-            new SwervePodState(vFR / max, aFR),
-            new SwervePodState(vFL / max, aFL),
+            new SwervePodState(vBR, aBR),
+            new SwervePodState(vBL, aBL),
+            new SwervePodState(vFR, aFR),
+            new SwervePodState(vFL, aFL),
         };
     }
 }
